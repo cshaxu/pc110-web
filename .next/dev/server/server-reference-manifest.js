3d76db176a6ac490f7ef5da1f46d6a3af218467a
cshaxu <cshaxu@gmail.com>
2026-08-15 14:21:38 -0700

    M0 Td S9 P1: ignore Next build output

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"5XVKzfUSjrQVVS7zBJb4piF8cROrTiicofh++jkgBCs=\"\n}"
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Y15A7wJLl7/6528on323ajiHYG00IyjXJS8cBaDvQqA=\"\n}"
